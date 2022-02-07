adab97160b8ff6552f28fe44d5b2c54690349c4e
YKizou <62426491+YKizou@users.noreply.github.com>
2022-02-08 00:07:45 +0100

    Modify Right Bar v2

--- client/src/components/Rightbar.jsx
+++ client/src/components/Rightbar.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import { Users } from '../dummyData'
-const PF = process.env.REACT_APP_PUBLIC_FOLDER
-const PB_ADS = PF + "ad.png"
-const PB_GIFT = PF + "gift.png"
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-const Rightbar = () => {
-  let connectedUsers = []
-  for(let i =0; i<2; i++){
-    connectedUsers.push(          
-    <div key={i} className="flex pt-3">
-      <img src={PF+Users[i].profilePicture} alt="" className="topbarImg cursor-pointer h-12 w-12 rounded-full object-cover"/>
-      <span className="topbarIconBadge rounded-full w-6 h-6 ml-7 absolute -z-100 bg-green-500 border-4 border-white justify-center"></span>
-      <code className="font-bold pl-3 text-sm self-center">{Users[i].username}</code>
-    </div> )
-  }
+const Rightbar = ({user}) => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER
+  const [listFriends, setListFriends]  = useState([])
+  var userFriends = []
+  const PB_ADS = PF + "ad.png"
+  const PB_GIFT = PF + "gift.png"
+
+
+  useEffect(()=>{
+    const fetchFollowings = async () => {
+      console.log(user.user)
+      await user.user.followings.map( async u => {
+        const res = await axios.get("http://localhost:8800/api/users/"+u)
+        userFriends.push(res.data)
+        userFriends.map( u => { 
+          var profileUrl = "/profile/" + u._id
+          setListFriends(oldArray=>[...oldArray,
+          <div key={u._id} className="flex flex-col pt-3">
+          <a href={profileUrl}><img src={PF+ u.profilePicture} alt="" className="topbarImg cursor-pointer h-28 w-28 rounded object-cover"/></a>
+          <code className="font-bold text-sm">{u.username}</code>
+          </div>
+        ]) 
+        })
+      })
+    };
+    fetchFollowings()
+  }, [user])
+
+
   return (
     <div className='rightbar pt-5 w-1/4 overflow-y-scroll px-2'>
       <div className='rightbarBirthday flex pb-7'>
@@ -22,10 +38,12 @@ const Rightbar = () => {
       </div>
       <img src={PB_ADS} alt="" className="rightbarAd rounded-2xl" />
       <div className="rightbarFriends pt-7">
-        <div className="rightbarListFriends">
-          <code className="font-bold text-lg">Online Friends</code>
-          {connectedUsers}
+        <div className="userFriends">
+        <code className='userFriends font-bold'>User Friends</code>
+        <div className="listuserFriends grid grid-cols-2">
+          { listFriends ? listFriends : null } 
         </div>
+      </div>
       </div>
     </div>
   )
