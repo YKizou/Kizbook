bda60fa507fd9f561dd95841645dd90c26f391a7
YKizou <62426491+YKizou@users.noreply.github.com>
2022-02-08 06:30:53 +0100

    Add user to local storage

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -5,6 +5,7 @@ export const loginCall = async (userCredential, dispatch) => {
   try{
     const res = await axios.post("/api/auth/login", userCredential);
     dispatch({type: "LOGIN_SUCCESS", payload: res.data })
+    localStorage.setItem("user", JSON.stringify(res.data))
   }catch(err){
     dispatch({type: "LOGIN_FAILURE", payload: err })
   }
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,17 +2,7 @@ import { createContext, useReducer } from "react"
 import AuthReducer  from "./AuthReducer"
 
 const INITIAL_STATE = {
-  user: {
-    _id:"61ca4100ee1f904579382044",
-    username:"youssefkizou",
-    email:"youssefkizou@gmail.com",
-    followings: ["61ca412eee1f904579382048", "61ca4119ee1f904579382046"],
-    password: "$2b$10$NUmgo2bqW02cRQb1SeGki.6j5O4wtr9BCllPo9ff32WzqWjSoa4w.",
-    profilePicture:"person/kizou.jpeg",
-    city:"evry",
-    desc:"Hello! My name is Youssef. Born and raised in Morocco, I now live in Paris. I am always curious and willing to learn new things."
-}
-  ,
+  user: JSON.parse(localStorage.getItem("user")),
   isFetching: false,
   error: false
 }
